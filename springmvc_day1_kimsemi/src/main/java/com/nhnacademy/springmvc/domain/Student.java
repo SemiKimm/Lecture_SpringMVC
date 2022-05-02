6890070780b40c28760745dee0ff2ee4e6ded2f4
김세미 <semikimm.m@gmail.com>
2022-05-02 23:05:37 +0900

    [springmvc-day1-hw] Student & StudentRepositoryImpl 구현 완료(?)

--- springmvc_day1_kimsemi/src/main/java/com/nhnacademy/springmvc/domain/Student.java
+++ springmvc_day1_kimsemi/src/main/java/com/nhnacademy/springmvc/domain/Student.java
@@ -1,9 +1,27 @@
 package com.nhnacademy.springmvc.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class Student {
+    @Getter
     private long id;
+    @Getter
     private String name;
+    @Getter
+    @Setter
     private String email;
+    @Getter
+    @Setter
     private int score;
+    @Getter
+    @Setter
     private String comment;
+
+    public static Student create(Long id, String name){return new Student(id, name);}
+
+    private Student(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
 }
--- springmvc_day1_kimsemi/src/main/java/com/nhnacademy/springmvc/repository/StudentRepositoryImpl.java
+++ springmvc_day1_kimsemi/src/main/java/com/nhnacademy/springmvc/repository/StudentRepositoryImpl.java
@@ -1,20 +1,33 @@
 package com.nhnacademy.springmvc.repository;
 
 import com.nhnacademy.springmvc.domain.Student;
+import java.util.HashMap;
+import java.util.Map;
 
-public class StudentRepositoryImpl implements StudentRepository{
+public class StudentRepositoryImpl implements StudentRepository {
+    private final Map<Long, Student> studentMap = new HashMap<>();
+    private long id = 0L;
+
     @Override
     public boolean exists(long id) {
-        return false;
+        return this.studentMap.containsKey(id);
     }
 
     @Override
     public Student register(String name, String email, int score, String comment) {
-        return null;
+        id++;
+        Student student = Student.create(id, name);
+        student.setEmail(email);
+        student.setScore(score);
+        student.setComment(comment);
+
+        this.studentMap.put(id, student);
+
+        return student;
     }
 
     @Override
     public Student getStudent(long id) {
-        return null;
+        return exists(id) ? studentMap.get(id) : null;
     }
 }
