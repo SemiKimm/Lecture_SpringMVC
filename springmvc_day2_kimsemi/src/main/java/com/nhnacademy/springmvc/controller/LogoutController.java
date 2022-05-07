cb174550a2861233f8256102e7586774d322be5f
김세미 <semikimm.m@gmail.com>
2022-05-07 23:09:08 +0900

    [springmvc-day2-hw] 로그아웃 완료(?)

--- springmvc_day2_kimsemi/src/main/java/com/nhnacademy/springmvc/controller/LogoutController.java
+++ springmvc_day2_kimsemi/src/main/java/com/nhnacademy/springmvc/controller/LogoutController.java
@@ -1,10 +1,34 @@
 package com.nhnacademy.springmvc.controller;
 
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Optional;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/logout")
 public class LogoutController {
-    @
+    @PostMapping
+    public String logout(HttpServletRequest request,
+                         HttpServletResponse response) {
+        Cookie[] cookies = request.getCookies();
+        Cookie sessionCookie =
+            Optional.of(Arrays.stream(cookies)
+                .filter(cookie -> cookie.getName().equals("SESSION"))
+                .findFirst()
+                .get()).orElse(null);
+
+        if (Objects.nonNull(sessionCookie)
+            && Objects.nonNull(sessionCookie.getValue())) {
+            Cookie deleteCookie = new Cookie("SESSION", null);
+            deleteCookie.setMaxAge(0);
+            response.addCookie(deleteCookie);
+        }
+        return "redirect:/login";
+    }
 }
